4ebad16066339ecdd19a9cc7a5f7cfad3ce3accd
antoine <antoine@camerlo.org>
2020-11-12 11:47:24 +0100

    Initial commit

---
+++ tunnel.sh
@@ -0,0 +1,8 @@
+CP=10.206.115.225
+scp ubuntu@${CP}:.kube/config kubeconfig
+export KUBECONFIG=$(pwd)/kubeconfig
+export OCTANT_PLUGIN_PATH=$(pwd)/octant/plugin
+kubectl config set clusters.kubernetes.server https://127.0.0.1:6443
+kubectl config set clusters.kubernetes.tls-server-name $CP
+ssh -N -L 6443:localhost:6443 ubuntu@$CP &
+./octant/octant &
